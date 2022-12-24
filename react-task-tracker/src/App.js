26c49478e79c8fd7964c5989c9cdfe17e4b97860
Tommy Wong <lokhim128@gmail.com>
2022-12-24 06:43:55 +0000

    Update App.js

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,56 +1,69 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
-import AddTask from './components/AddTask';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
-  
-  const [tasks, setTasks] = useState([ // *Always keep the state in App.js
+  const [tasks, setTasks] = useState([
+    // *Always keep the state in App.js
     {
       id: 1,
-      content: 'content01',
-      date: 'day01',
-      reminder: false
+      content: "content01",
+      date: "day01",
+      reminder: false,
     },
     {
       id: 2,
-      content: 'content02',
-      date: 'day02',
-      reminder: false
+      content: "content02",
+      date: "day02",
+      reminder: false,
     },
     {
       id: 3,
-      content: 'content03',
-      date: 'day03',
-      reminder: false
-    }
+      content: "content03",
+      date: "day03",
+      reminder: false,
+    },
   ]);
 
   const addTaskHandler = (task) => {
     console.log(task);
-  }
+  };
 
   const deleteTaskHandler = (selectedId) => {
-    console.log('deleteTaskHandler: ', selectedId);
+    console.log("deleteTaskHandler: ", selectedId);
 
     setTasks(tasks.filter((task) => task.id !== selectedId));
-  }
+  };
 
   const toggleHandler = (selectedId) => {
-    console.log('toggleHandler: ', selectedId);
+    console.log("toggleHandler: ", selectedId);
 
-    setTasks(tasks.map((task) => task.id === selectedId ? {...task, reminder: !task.reminder} : task));
-  }
+    setTasks(
+      tasks.map((task) =>
+        task.id === selectedId ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  // test
 
   return (
     <div className="container">
       <Header />
-      <AddTask onAdd={addTaskHandler}/>
-      { tasks.length === 0 ? 'No tasks here' : <Tasks tasks={tasks} onDelete={deleteTaskHandler} onToggle={toggleHandler} /> }
+      <AddTask onAdd={addTaskHandler} />
+      {tasks.length === 0 ? (
+        "No tasks here"
+      ) : (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTaskHandler}
+          onToggle={toggleHandler}
+        />
+      )}
     </div>
   );
-
-}
+};
 
 export default App;
